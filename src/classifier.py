562ad7567fd2a5bd9b925d5ec87f6e610eb153b7
nickchengtw <33611456+nickchengtw@users.noreply.github.com>
2025-06-05 01:17:21 +0800

    Chore: cleanup unused file and code

--- src/classifier.py
+++ src/classifier.py
@@ -1,33 +1,43 @@
+import argparse
+
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, roc_auc_score
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from sklearn.metrics import classification_report
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.utils import to_categorical
+import matplotlib
+matplotlib.use('TkAgg')  # Or 'QtAgg' depending on your system
 
 from src.preprocess.feature import *
 from src.preprocess.make_dataset import *
 from src.utils.plot import plot_labeled
 
-import matplotlib
-matplotlib.use('TkAgg')  # Or 'QtAgg' depending on your system
 
-# Load the data
-df_det = pd.read_csv('data/data.csv', parse_dates=['Date'], index_col=0, dtype={'Up Trend': 'object', 'Down Trend': 'object'})[:180]
-windows, labels = get_window_dataset(df_det)
+# Argument parser
+parser = argparse.ArgumentParser(description="Trend prediction with CNN")
+parser.add_argument('-c', '--csv_path', type=str, default='data/data.csv', help='Path to input CSV file')
+parser.add_argument('-m', '--model_path', type=str, default='models/cnn_1d_huge.keras', help='Path to trained model')
+parser.add_argument('-l', '--limit', type=int, default=180, help='Number of rows to load from CSV')
 
+args = parser.parse_args()
+
+# Constants
 WINDOW_SIZE = 15
 MIXED_LABEL = 1
+
+# Load the data
+df_det = pd.read_csv(
+    args.csv_path,
+    parse_dates=['Date'],
+    index_col=0,
+    dtype={'Up Trend': 'object', 'Down Trend': 'object'}
+)[:args.limit]
+
+windows, labels = get_window_dataset(df_det)
 
 X = np.array(windows)
 X = normalize_windows(X)
 
-# Step 1: Predict class probabilities
-model = tf.keras.models.load_model('models/cnn_1d_huge.keras')
+# Load model
+model = tf.keras.models.load_model(args.model_path)
 y_pred_probs = model.predict(X)
 # Step 2: Convert predictions and true labels to class indices
 y_pred = np.argmax(y_pred_probs, axis=1)
